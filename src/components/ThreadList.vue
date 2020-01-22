a38c4989dca8f8a690ff428bbc02b079ebda068a
Alfredo Stanquini Neto <alfredo.stanquini@doc88.com.br>
2020-01-22 18:33:50 -0300

    adiciona componentes de threads

---
+++ src/components/ThreadList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="thread-list">
+
+    <h2 class="list-title">Threads</h2>
+    <ThreadListItem/>
+  </div>
+</template>
+
+<script>
+import ThreadListItem from './ThreadListItem'
+
+export default {
+  components: {
+    ThreadListItem
+  }
+};
+</script>
---
+++ src/components/ThreadListItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="thread">
+    <div>
+      <p>
+        <a href="thread.html">Wasabi vs horseraddish?</a>
+      </p>
+      <p class="text-faded text-xsmall">
+        By <a href="profile.html">Robin</a>, 8 hours ago
+      </p>
+    </div>
+    <div class="activity">
+      <p class="replies-count">
+        3 replies
+      </p>
+      <img
+        class="avatar-medium"
+        src="https://firebasestorage.googleapis.com/v0/b/forum-2a982.appspot.com/o/images%2Favatars%2Fraynathan?alt=media&amp;token=bd9a0f0e-60f2-4e60-b092-77d1ded50a7e"
+        alt=""
+      />
+      <span>
+        <a class="text-xsmall" href="profile.html">Ray-Nathan James</a>
+        <p class="text-faded text-xsmall">3 hours ago</p>
+      </span>
+    </div>
+  </div>
+</template>
